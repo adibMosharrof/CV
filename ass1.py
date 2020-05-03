56c45b6f1a09be83b8178c2297cabb52ca4542e0
Adib Mosharrof <adib.mosharrof@gmail.com>
2020-05-03 01:24:08 -0700

    kmeans works properly

--- ass1.py
+++ ass1.py
@@ -32,6 +32,7 @@ classes = ('plane', 'car', 'bird', 'cat',
 
 img_rows, img_cols = 32, 32
 input_shape = (img_rows, img_cols, 3)
+
 def load_pickle(f):
     version = platform.python_version_tuple()
     if version[0] == '2':
@@ -67,12 +68,8 @@ def load_CIFAR10(ROOT):
 
 def get_CIFAR10_data(num_training=490, num_validation=10, num_test=100):
     # Load the raw CIFAR-10 data
-    env = get_env_name()
-    if env == "local":
-        cifar10_dir = 'E:/Study/CV/cifar-10-batches-py'
-    else:
-        cifar10_dir = '/home/adibm/uoml/adib/CV/cifar-10-batches-py'
-        
+    cifar10_dir = './cifar-10-batches-py'
+    
     X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
 
     # Subsample the data
@@ -93,24 +90,9 @@ def get_CIFAR10_data(num_training=490, num_validation=10, num_test=100):
     x_test /= 255
 
     return x_train, y_train, X_val, y_val, x_test, y_test
-
-
 """
 End of methods for loading the dataset
 """
-def get_env_name():
-    hostname = socket.gethostname()
-    if hostname == "LAPTOP-DNMQC5VO":
-        return "local"
-    elif "uomlmedifor" in hostname:
-        return "openstack" 
-    elif re.search("(?<=n).\d+", hostname) is not None:
-        return "talapas" 
-    else:
-        ValueError(f"Need to configure the new hostname {hostname}")
-
-
-
 
 def predict(x_train,x_test, y_train, distance_func):
     predictions = []  # placeholder for N labels
@@ -129,33 +111,61 @@ def predict(x_train,x_test, y_train, distance_func):
     return predictions
 
 def kmeans():
-    K=3
-    start = time.time()
-    #X_i = LazyTensor(x_test[:, None, :])  # (10000, 1, 784) test set
-    #X_j = LazyTensor(x_train[None, :, :])  # (1, 60000, 784) train set
-    # D_ij = ((X_i - X_j) ** 2).sum(-1)  # (10000, 60000) symbolic matrix of squared L2 distances
+    x_train, y_train, x_val, y_val, x_test, y_test = get_CIFAR10_data()
     pred1 = predict(x_train, x_test, y_train, np.abs) 
     pred2 = predict(x_train, x_test, y_train, np.square) 
     
-    # ind_knn = D_ij.argKmin(K, dim=1)  # Samples <-> Dataset, (N_test, K)
-    # lab_knn = y_train[ind_knn]  # (N_test, K) array of integers in [0,9]
-    # y_knn, _ = pred.mode()   # Compute the most likely label
-    end = time.time()
-    
     accuracy1 = (pred1 == y_test).astype(float).mean().item()
     accuracy2 = (pred2 == y_test).astype(float).mean().item()
-    # accuracy1 = 1.0-error1
-    # accuracy2 = 1.0-error2
-    time  = end - start
     
-    print("K-NN on CIFAR dataset with L1 distance: test accuracy = {:.2f}% in {:.2f}s.".format(accuracy1*100, time))
-    print("K-NN on CIFAR dataset with L2 distance: test accuracy = {:.2f}% in {:.2f}s.".format(accuracy2*100, time))
+    print("K-NN on CIFAR dataset with L1 distance: test accuracy = {:.2f}".format(accuracy1*100))
+    print("K-NN on CIFAR dataset with L2 distance: test accuracy = {:.2f}".format(accuracy2*100))
 
 def imshow(img):
     img = img / 2 + 0.5     # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
+
+
+class LizardCnn(nn.Module):
+    def __init__(self):
+        super(LizardCnn,self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=12, kernel_size=5)
+        
+        self.fc1 = nn.Linear(in_features=12*5*5, out_features=120)
+        self.fc2 = nn.Linear(in_features=120, out_features=60)
+        self.out = nn.Linear(in_features=60, out_features=10)
+        
+    def forward(self, t):
+        # (1) input layer
+        t = t
+        
+        # (2) hidden conv layer
+        t = self.conv1(t)
+        t = F.relu(t)
+        t = F.max_pool2d(t, kernel_size=2, stride=2)
+        
+        # (3) hidden conv layer
+        t = self.conv2(t)
+        t = F.relu(t)
+        t = F.max_pool2d(t, kernel_size=2, stride=2)
+        
+        # (4) hidden linear layer
+        t = t.reshape(-1, 12 * 5 * 5)
+        t = self.fc1(t)
+        t = F.relu(t)
+        
+        # (5) hidden linear layer
+        t = self.fc2(t)
+        t = F.relu(t)
+        
+        # (6) output layer
+        t = self.out(t)
+        #t = F.softmax(t, dim=1)
+        
+        return t
 
 class Cnn(nn.Module):
     def __init__(self):
@@ -175,6 +185,45 @@ class Cnn(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+    
+
+
+class SimpleCnn(nn.Module):
+    def __init__(self):
+        super(SimpleCnn, self).__init__()
+        
+        #Input channels = 3, output channels = 18
+        self.conv1 = torch.nn.Conv2d(3, 18, kernel_size=3, stride=1, padding=1)
+        self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        
+        #4608 input features, 64 output features (see sizing flow below)
+        self.fc1 = torch.nn.Linear(18 * 16 * 16, 64)
+        
+        #64 input features, 10 output features for our 10 defined classes
+        self.fc2 = torch.nn.Linear(64, 10)
+
+    def forward(self, x):
+        
+        #Computes the activation of the first convolution
+        #Size changes from (3, 32, 32) to (18, 32, 32)
+        x = F.relu(self.conv1(x))
+        
+        #Size changes from (18, 32, 32) to (18, 16, 16)
+        x = self.pool(x)
+        
+        #Reshape data to input to the input layer of the neural net
+        #Size changes from (18, 16, 16) to (1, 4608)
+        #Recall that the -1 infers this dimension from the other given dimension
+        x = x.view(-1, 18 * 16 *16)
+        
+        #Computes the activation of the first fully connected layer
+        #Size changes from (1, 4608) to (1, 64)
+        x = F.relu(self.fc1(x))
+        
+        #Computes the second fully connected layer (activation applied later)
+        #Size changes from (1, 64) to (1, 10)
+        x = self.fc2(x)
+        return(x)
 
 def part2():
     transform = transforms.Compose(
@@ -200,7 +249,9 @@ def part2():
     # print labels
     print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
     
-    cnn = Cnn()
+#     cnn = Cnn()
+#     cnn = SimpleCnn()
+    cnn = LizardCnn()
     train(cnn,trainloader)
     dataiter = iter(testloader)
     images, labels = dataiter.next()
@@ -252,19 +303,8 @@ def train(net, trainloader):
 
     print('Finished Training')
 
-def init():
-    x_train, y_train, x_val, y_val, x_test, y_test = get_CIFAR10_data()
-    
-    
-    print('Train data shape: ', x_train.shape)
-    print('Train labels shape: ', y_train.shape)
-    print('Validation data shape: ', x_val.shape)
-    print('Validation labels shape: ', y_val.shape)
-    print('Test data shape: ', x_test.shape)
-    print('Test labels shape: ', y_test.shape)
 
 if __name__ == '__main__':
-    init()
-    #kmeans()
-    part2()
+    kmeans()
+#     part2()
 
